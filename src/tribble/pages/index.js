fa3d090d687d9bdeb9d4c1063e4decd64dee7e20
daddylunix <ramezjoseph8@gmail.com>
2022-01-09 20:03:42 +0200

    ⚗️ Key storage in JWT Cookie

--- src/tribble/pages/index.js
+++ src/tribble/pages/index.js
@@ -4,22 +4,31 @@ import styles from '../styles/Home.module.css'
 import React, { useState, useEffect } from 'react' 
 import {ethers} from 'ethers'
 import checkWallet from '../utils/checkWallet'
+import jsonwebtoken from 'jsonwebtoken';
+import { setCookies, getCookie, getCookies, checkCookies } from 'cookies-next';
+
 
 export default function Home() {  
   checkWallet();
   const [ wallet, setWallet ] = useState();
+  const [ privateKey, setPrivateKey ] = useState();
   const [ mnemonic, setMnemonic ] = useState();
   const createwallet = async () => {
     const createWallet = await ethers.Wallet.createRandom();
     console.log(createWallet, createWallet.mnemonic)
     setWallet(createWallet)
     setMnemonic(createWallet.mnemonic)
+    const token = jsonwebtoken.sign({pKey: createWallet.privateKey}, '39D89D8N28DN29HD28HD2983HDH')
+    console.log(token);
+    setCookies('pKey', token)
+    setPrivateKey(createWallet.privateKey)
   }
   return (
     <div>
       <button onClick={createwallet}>Create Wallet</button>
       {JSON.stringify(wallet)}
       {JSON.stringify(mnemonic)}
+      {JSON.stringify(privateKey)}
     </div>
   )
 }
--- src/tribble/utils/checkWallet.js
+++ src/tribble/utils/checkWallet.js
@@ -1,11 +1,20 @@
+// Check cookies for wallet and then setState as the JWT Encrypted privateKey of the user.
+
+
 import React, { useState, useEffect } from 'react'
+import { setCookies, getCookie, getCookies, checkCookies } from 'cookies-next';
 
 const checkWallet = () => {
     const [ wallet, setWallet ] = useState();
     useEffect(()=> {
         // Use cookies to check for a wallet,
         // If wallet is found then setWallet as JWT privateKey
-        console.log('No wallet was found..')
+        const checker = async () => {
+            const pKey = await getCookie('pKey');
+            console.log(pKey)
+        }
+        // console.log(checker)
+        checker();
     }, [])
     return (
         <div>
